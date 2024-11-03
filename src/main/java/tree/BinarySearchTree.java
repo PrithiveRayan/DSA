22f8c09362081bef9f2031a8466405ccfb111914
prithive <prithiverayan777@gmail.com>
2024-11-03 20:34:52 +0530

    DSA

--- src/main/java/tree/BinarySearchTree.java
+++ src/main/java/tree/BinarySearchTree.java
@@ -1,11 +1,16 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BinarySearchTree<T extends Comparable<T>> {
 
 
 
     Node root;
-
+    int maxDiameter = 0;
     public BinarySearchTree(T data){
         root = new Node(data);
     }
@@ -67,6 +72,32 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
     }
 
+    public void inOrderIteratively(){
+        inOrderIteratively(root);
+        System.out.println();
+    }
+
+    private void inOrderIteratively(Node root){
+        List<Integer> inOrder = new LinkedList<>();
+        Stack<Node> stack = new Stack<>();
+        Node node = root;
+        while(true){
+            if(node != null){
+                stack.push(node);
+                node = node.left;
+            }
+            else{
+                if(stack.isEmpty()){
+                    break;
+                }
+                node = stack.pop();
+                inOrder.add((Integer) node.data);
+                node = node.right;
+            }
+        }
+        System.out.println(inOrder);
+    }
+
     public void preOrder(){
         preOrder(root);
         System.out.println();
@@ -79,8 +110,33 @@ public class BinarySearchTree<T extends Comparable<T>> {
             preOrder(root.right);
         }
     }
+
+    public void preOrderIteratively(){
+        preOrderIteratively(root);
+        System.out.println();
+    }
+
+    private void preOrderIteratively(Node root){
+       List<Integer> preOrder = new LinkedList<>();
+       if(root == null)
+           System.out.println("Nothing to print");
+       Stack<Node> stack = new Stack<>();
+       stack.push(root);
+       while(!stack.empty()){
+           root = stack.pop();
+           preOrder.add((Integer) root.data);
+           if(root.right != null){
+               stack.push(root.right);
+           }
+           if(root.left != null){
+               stack.push(root.left);
+           }
+       }
+        System.out.println(preOrder);
+    }
+
     public void postOrder(){
-        inOrder(root);
+        postOrder(root);
         System.out.println();
     }
 
@@ -90,6 +146,63 @@ public class BinarySearchTree<T extends Comparable<T>> {
             postOrder(root.right);
             System.out.print(root.data+" ");
         }
+    }
+
+    public void postOrderIteratively(){
+        postOrderIteratively(root);
+        System.out.println();
+    }
+
+    private void postOrderIteratively(Node root){
+        List<Integer> postOrder = new LinkedList<>();
+        Stack<Node> left = new Stack<>();
+        Stack<Node> right = new Stack<>();
+        if(root == null)
+            System.out.println("Nothing to print");
+        left.push(root);
+        while(!left.empty()){
+            root = left.pop();
+            right.add(root);
+            if(root.left != null){
+                left.push(root.left);
+            }
+            if(root.right != null){
+                left.push(root.right);
+            }
+        }
+        while (!right.isEmpty()){
+            postOrder.add((Integer) right.pop().data);
+        }
+        System.out.println(postOrder);
+    }
+
+
+    public void levelOrder(){
+        levelOrder(root);
+        System.out.println();
+    }
+
+    private void levelOrder(Node root){
+        Queue<Node> queue = new LinkedList<>();
+        List<List<Integer>> result = new LinkedList<>();
+        if(root == null)
+            System.out.println("No elements to print.");
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> orders = new LinkedList<>();
+            for(int i=0; i<size; i++){
+                if(queue.peek().left != null){
+                    queue.offer(queue.peek().left);
+                }
+                if(queue.peek().right != null){
+                    queue.offer(queue.peek().right);
+                }
+                orders.add((Integer) queue.poll().data);
+            }
+            result.add(orders);
+        }
+        System.out.println(result);
     }
 
     public boolean findMax(T first, T second) {
@@ -149,6 +262,55 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
         int left = findDFSDepth(root.left);
         int right = findDFSDepth(root.right);
+        return 1+Math.max(right,left);
+    }
+
+    public boolean isBalancedTree(){
+        return isBalancedTree(root) != -1;
+    }
+
+    private int isBalancedTree(Node root){
+        if(root == null){
+            return 0;
+        }
+        int left = isBalancedTree(root.left);
+        if(left == -1) return -1;
+        int right = isBalancedTree(root.right);
+        if(right == -1) return -1;
+        if(Math.abs(left-right) > 1)
+            return -1;
+        return 1+Math.max(right,left);
+    }
+
+//    public int diameterOfATree(){
+//        int[] diameter = new int[1];
+//        diameterOfATree(root, diameter);
+//        return diameter[0];
+//    }
+//
+//    private int diameterOfATree(Node root, int[] diameter){
+//        if(root == null){
+//            return 0;
+//        }
+//        int left = diameterOfATree(root.left, diameter);
+//        int right = diameterOfATree(root.right,diameter);
+//        diameter[0] = Math.max(diameter[0], left+right);
+//        return 1+Math.max(right,left);
+//    }
+
+    public int diameterOfATree(){
+        maxDiameter = 0;
+        diameterOfATree(root, maxDiameter);
+        return maxDiameter;
+    }
+
+    private int diameterOfATree(Node root, int diameter){
+        if(root == null){
+            return 0;
+        }
+        int left = diameterOfATree(root.left, diameter);
+        int right = diameterOfATree(root.right,diameter);
+        diameter = Math.max(diameter, left+right);
         return 1+Math.max(right,left);
     }
 
--- src/main/java/tree/DriverClass.java
+++ src/main/java/tree/DriverClass.java
@@ -9,9 +9,17 @@ public class DriverClass {
         tree.insert(10);
         tree.insert(5);
         tree.insert(8);
-        System.out.println("Maximum depth is "+tree.findDFSMaxDepth());
 
-        tree.inOrder();
+
+        System.out.println("Maximum depth is "+tree.findDFSMaxDepth());
+        tree.levelOrder();
+//        tree.preOrder();
+//        tree.preOrderIteratively();
+//        tree.postOrder();
+//        tree.postOrderIteratively();
+//        tree.inOrder();
+//        tree.inOrderIteratively();
+
 
         if(tree.search(10) == null){
             System.out.println("Element not found.");
@@ -21,8 +29,10 @@ public class DriverClass {
         }
 
         tree.delete(4);
-
         tree.inOrder();
+
+//        System.out.println("Is Balanced tree? "+tree.isBalancedTree());
+        System.out.println("Diameter of a tree is "+tree.diameterOfATree());
 
 
     }
